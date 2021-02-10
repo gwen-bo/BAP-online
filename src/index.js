f7a15d7ecba5b62303293f72b86cadad7b38c6ef
gwen-bo <gwen.bogaert@hotmail.com>
2021-02-10 21:06:36 +0100

    added custom cursor, optimised all pictures

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+import AnimatedCursor from "react-animated-cursor"
+
 import ScrollToTop from "./components/ScrollPosition/index.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
         <ScrollToTop />
+        <AnimatedCursor
+        innerSize={15}
+        outerSize={10}
+        color='242, 166, 85'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={10}
+        />
         <App />
     </BrowserRouter>
   </React.StrictMode>,
